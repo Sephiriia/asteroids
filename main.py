65d14c476a537e977451a69dcc0cc01fcc7249e4
Sephiiria <lassebkr@protonmail.com>
2025-01-13 10:23:33 +0100

    B: Optimized resource usage

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from constants import *
 
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     pygame.display.set_caption("Asteroids")
 
@@ -14,12 +16,15 @@ def main():
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                running = False
+                return
 
         # Step 3: Draw the game onto the screen
         screen.fill((0, 0, 0))  # Fill the screen with black
 
         pygame.display.flip()  # Update the full display Surface to the screen
+
+        dt = clock.tick(60) / 1000
+
 
     pygame.quit()
 
