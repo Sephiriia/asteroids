d034cac687e5be78151b1307bc110681bd3626c7
Sephiiria <lassebkr@protonmail.com>
2025-01-13 14:25:21 +0100

    added left & right movement to game

--- main.py
+++ main.py
@@ -4,19 +4,14 @@ from player import Player
 
 def main():
     pygame.init()
-    clock = pygame.time.Clock()
-    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    pygame.display.set_caption("Asteroids")
-
+    clock = pygame.time.Clock()
 
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
 
-
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)    
-
 
     running = True
     while running:
@@ -24,15 +19,17 @@ def main():
             if event.type == pygame.QUIT:
                 return
 
+        dt = clock.tick(60) / 1000
+
+        # Update the player
+        player.update(dt)
+
         # Step 3: Draw the game onto the screen
         screen.fill((0, 0, 0))  # Fill the screen with black
 
         player.draw(screen)
 
         pygame.display.flip()  # Update the full display Surface to the screen
-
-        dt = clock.tick(60) / 1000
-
 
     pygame.quit()
 
